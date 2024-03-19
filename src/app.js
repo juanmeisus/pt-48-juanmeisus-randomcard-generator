594432163a47025824ff56b51da7cf9f109b4fe8
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2024-03-19 13:48:02 +0000

    ultimos cambios con onload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardElement = document.querySelector("#theCard");
+
+  let cardValues = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const cardSuits = ["heart", "spade", "club", "diamond"];
+
+  function randomCard() {
+    const valueIndex = Math.floor(Math.random() * cardValues.length);
+    const suitIndex = Math.floor(Math.random() * cardSuits.length);
+
+    const cardValue = cardValues[valueIndex];
+    const cardSuit = cardSuits[suitIndex];
+
+    cardElement.className = "card suit-" + cardSuit;
+    cardElement.querySelector("#cardContent").innerHTML = cardValue;
+  }
+
+  randomCard();
 };
